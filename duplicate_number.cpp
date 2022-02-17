edd0230965f159dd4e1679fd863f61b6d4642fa5
suyashjhawer2k <79996416+suyashjhawer2k@users.noreply.github.com>
2022-02-17 15:20:26 +0530

    Create duplicate_number.cpp

---
+++ duplicate_number.cpp
@@ -0,0 +1,10 @@
+int findDuplicate(vector<int>& nums) {
+        for(int i=0 ; i<nums.size() ; i++)
+        {
+            int id=abs(nums[i])-1 ;
+            nums[id]=nums[id]*-1;
+            if(nums[id]>0)
+                return abs(nums[i]) ;
+        }
+        return -1 ;
+    }
